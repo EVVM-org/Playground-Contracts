33c1efb322a2d7dda3a7dbb07e7eab008cb08062
jistro <contact@jistro.xyz>
2025-05-12 17:44:48 -0600

    fix: fix deploy script and gitignore

--- script/DeployMock.s.sol
+++ script/DeployMock.s.sol
@@ -2,11 +2,11 @@
 pragma solidity ^0.8.13;
 
 import {Script, console2} from "forge-std/Script.sol";
-import {EvvmTesnet} from "Tesnet-contracts/EvvmTesnet.sol";
-import {SMateTesnet} from "Tesnet-contracts/SMateTesnet.sol";
+import {EvvmMock} from "mock-contracts/EvvmMock.sol";
+import {SMateMock} from "mock-contracts/SMateMock.sol";
 
-contract DeployTestnet is Script {
-    SMateTesnet sMate;
+contract DeployMock is Script {
+    SMateMock sMate;
 
     function run() public {
         vm.broadcast();
@@ -14,12 +14,12 @@ contract DeployTestnet is Script {
     }
 
     function deployEvvm() public returns (address sMateAddress) {
-        sMate = new SMateTesnet(msg.sender);
+        sMate = new SMateMock(msg.sender);
         console2.log("sMate address: ", address(sMate));
         console2.log("Evvm address: ", sMate.getEvvmAddress());
         console2.log(
             "MNS address: ",
-            EvvmTesnet(sMate.getEvvmAddress()).getMateNameServiceAddress()
+            EvvmMock(sMate.getEvvmAddress()).getMateNameServiceAddress()
         );
         return address(sMate);
     }
