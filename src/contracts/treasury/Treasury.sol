a3cb7c69fb7c00bb3cb16ee6d80f971c66a8d9a6
jistro <contact@jistro.xyz>
2025-08-20 19:36:47 -0600

    feat: enhance Treasury contract with unified deposit and withdraw functions, adding error handling for insufficient balance and principal token restrictions

--- src/contracts/treasury/Treasury.sol
+++ src/contracts/treasury/Treasury.sol
@@ -16,6 +16,7 @@ pragma solidity ^0.8.0;
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeTransferLib} from "@solady/utils/SafeTransferLib.sol";
 import {Evvm} from "@EVVM/playground/contracts/evvm/Evvm.sol";
+import {ErrorsLib} from "@EVVM/playground/contracts/treasury/lib/ErrorsLib.sol";
 
 contract Treasury {
     address public evvmAddress;
@@ -24,23 +25,40 @@ contract Treasury {
         evvmAddress = _evvmAddress;
     }
 
-    function depositNativeHostToken() external payable {
-        Evvm(evvmAddress).addAmountToUser(msg.sender, address(0), msg.value);
+    function deposit(address token, uint256 amount) external payable {
+        if (msg.value > 0) { /// user is sending host native coin
+            Evvm(evvmAddress).addAmountToUser(
+                msg.sender,
+                address(0),
+                msg.value
+            );
+        } else { /// user is sending ERC20 tokens
+            IERC20(token).transferFrom(msg.sender, address(this), amount);
+            Evvm(evvmAddress).addAmountToUser(msg.sender, token, amount);
+        }
     }
 
-    function depositERC20HostToken(address token, uint256 amount) external {
-        IERC20(token).transferFrom(msg.sender, address(this), amount);
-        Evvm(evvmAddress).addAmountToUser(msg.sender, token, amount);
-    }
+    function withdraw(address token, uint256 amount) external {
+        if (Evvm(evvmAddress).getBalance(msg.sender, token) < amount)
+            revert ErrorsLib.InsufficientBalance();
 
-    function withdrawNativeHostToken(uint256 amount) external {
-        Evvm(evvmAddress).removeAmountFromUser(msg.sender, address(0), amount);
-        SafeTransferLib.safeTransferETH(msg.sender, amount);
-    }
+        if (token == Evvm(evvmAddress).getEvvmMetadata().principalTokenAddress)
+            revert ErrorsLib.PrincipalTokenIsNotWithdrawable();
 
-    function withdrawERC20HostToken(address token, uint256 amount) external {
-        Evvm(evvmAddress).removeAmountFromUser(msg.sender, token, amount);
-        IERC20(token).transfer(msg.sender, amount);
-    }
+        if (token == address(0)) { /// user is trying to withdraw native coin
 
+            Evvm(evvmAddress).removeAmountFromUser(
+                msg.sender,
+                address(0),
+                amount
+            );
+            SafeTransferLib.safeTransferETH(msg.sender, amount);
+
+        } else { /// user is trying to withdraw ERC20 tokens
+
+            Evvm(evvmAddress).removeAmountFromUser(msg.sender, token, amount);
+            IERC20(token).transfer(msg.sender, amount);
+
+        }
+    }
 }
