1ffba8d29606f8cd64896aaf7bf201c97e0fee53
jistro <contact@jistro.xyz>
2025-10-25 18:56:32 -0600

    feat: add registration username flush tests and signature handling

--- test/unit/nameService/correct/unitTestCorrect_NameService_registrationUsername_SyncExecutionOnPay.t.sol
+++ test/unit/nameService/correct/unitTestCorrect_NameService_registrationUsername_SyncExecutionOnPay.t.sol
@@ -65,7 +65,10 @@ contract unitTestCorrect_NameService_registrationUsername_SyncExecutionOnPay is
 
         staking._setupEstimatorAndEvvm(address(estimator), address(evvm));
         treasury = new Treasury(address(evvm));
-        evvm._setupNameServiceAndTreasuryAddress(address(nameService), address(treasury));
+        evvm._setupNameServiceAndTreasuryAddress(
+            address(nameService),
+            address(treasury)
+        );
 
         evvm.setPointStaker(COMMON_USER_STAKER.Address, 0x01);
     }
@@ -160,6 +163,190 @@ contract unitTestCorrect_NameService_registrationUsername_SyncExecutionOnPay is
         signatureEVVM = Erc191TestBuilder.buildERC191Signature(v, r, s);
     }
 
+    function makeMakeOfferSignatures(
+        AccountData memory user,
+        string memory usernameToMakeOffer,
+        uint256 expireDate,
+        uint256 amountToOffer,
+        uint256 nonceNameService,
+        uint256 priorityFeeAmountEVVM,
+        uint256 nonceEVVM,
+        bool priorityFlagEVVM
+    )
+        private
+        view
+        returns (bytes memory signatureNameService, bytes memory signatureEVVM)
+    {
+        uint8 v;
+        bytes32 r;
+        bytes32 s;
+
+        (v, r, s) = vm.sign(
+            user.PrivateKey,
+            Erc191TestBuilder.buildMessageSignedForMakeOffer(
+                evvm.getEvvmID(),
+                usernameToMakeOffer,
+                expireDate,
+                amountToOffer,
+                nonceNameService
+            )
+        );
+        signatureNameService = Erc191TestBuilder.buildERC191Signature(v, r, s);
+
+        (v, r, s) = vm.sign(
+            user.PrivateKey,
+            Erc191TestBuilder.buildMessageSignedForPay(
+                evvm.getEvvmID(),
+                address(nameService),
+                "",
+                MATE_TOKEN_ADDRESS,
+                amountToOffer,
+                priorityFeeAmountEVVM,
+                nonceEVVM,
+                priorityFlagEVVM,
+                address(nameService)
+            )
+        );
+        signatureEVVM = Erc191TestBuilder.buildERC191Signature(v, r, s);
+    }
+
+    function makeFlushUsernameSignatures(
+        AccountData memory user,
+        string memory username,
+        uint256 nonceNameService,
+        uint256 priorityFeeAmountEVVM,
+        uint256 nonceEVVM,
+        bool priorityFlagEVVM
+    )
+        private
+        view
+        returns (bytes memory signatureNameService, bytes memory signatureEVVM)
+    {
+        uint8 v;
+        bytes32 r;
+        bytes32 s;
+
+        (v, r, s) = vm.sign(
+            user.PrivateKey,
+            Erc191TestBuilder.buildMessageSignedForFlushUsername(
+                evvm.getEvvmID(),
+                username,
+                nonceNameService
+            )
+        );
+        signatureNameService = Erc191TestBuilder.buildERC191Signature(v, r, s);
+
+        (v, r, s) = vm.sign(
+            user.PrivateKey,
+            Erc191TestBuilder.buildMessageSignedForPay(
+                evvm.getEvvmID(),
+                address(nameService),
+                "",
+                MATE_TOKEN_ADDRESS,
+                nameService.getPriceToFlushUsername(username),
+                priorityFeeAmountEVVM,
+                nonceEVVM,
+                priorityFlagEVVM,
+                address(nameService)
+            )
+        );
+        signatureEVVM = Erc191TestBuilder.buildERC191Signature(v, r, s);
+    }
+
+    modifier preparePostRegistrationAndFlush() {
+        addBalance(COMMON_USER_NO_STAKER_1.Address, "testflush", 0);
+        makePreRegistrationUsername(
+            COMMON_USER_NO_STAKER_1,
+            "testflush",
+            777,
+            1
+        );
+
+        skip(30 minutes);
+
+        (
+            bytes memory signatureNameService,
+            bytes memory signatureEVVM
+        ) = makeRegistrationUsernameSignatures(
+                COMMON_USER_NO_STAKER_1,
+                "testflush",
+                777,
+                2,
+                0,
+                2,
+                true
+            );
+
+        nameService.registrationUsername(
+            COMMON_USER_NO_STAKER_1.Address,
+            "testflush",
+            777,
+            2,
+            signatureNameService,
+            0,
+            2,
+            true,
+            signatureEVVM
+        );
+
+        evvm.addBalance(
+            COMMON_USER_NO_STAKER_2.Address,
+            MATE_TOKEN_ADDRESS,
+            1.67 ether
+        );
+
+        (signatureNameService, signatureEVVM) = makeMakeOfferSignatures(
+            COMMON_USER_NO_STAKER_2,
+            "testflush",
+            block.timestamp + 30 days,
+            1.67 ether,
+            3,
+            0,
+            3,
+            true
+        );
+
+        nameService.makeOffer(
+            COMMON_USER_NO_STAKER_2.Address,
+            "testflush",
+            block.timestamp + 30 days,
+            1.67 ether,
+            3,
+            signatureNameService,
+            0,
+            3,
+            true,
+            signatureEVVM
+        );
+
+        evvm.addBalance(
+            COMMON_USER_NO_STAKER_1.Address,
+            MATE_TOKEN_ADDRESS,
+            nameService.getPriceToFlushUsername("testflush")
+        );
+
+        (signatureNameService, signatureEVVM) = makeFlushUsernameSignatures(
+            COMMON_USER_NO_STAKER_1,
+            "testflush",
+            4,
+            0,
+            4,
+            true
+        );
+
+        nameService.flushUsername(
+            COMMON_USER_NO_STAKER_1.Address,
+            "testflush",
+            4,
+            signatureNameService,
+            0,
+            4,
+            true,
+            signatureEVVM
+        );
+        _;
+    }
+
     /**
      * Function to test:
      * nS: No staker
@@ -168,7 +355,9 @@ contract unitTestCorrect_NameService_registrationUsername_SyncExecutionOnPay is
      * nPF: No priority fee
      */
 
-    function test__unit_correct__registrationUsername__nS_nPF() external {
+    function test__unit_correct__registrationUsername__noFlush__nS_nPF()
+        external
+    {
         addBalance(COMMON_USER_NO_STAKER_1.Address, "test", 0);
         makePreRegistrationUsername(
             COMMON_USER_NO_STAKER_1,
@@ -227,7 +416,9 @@ contract unitTestCorrect_NameService_registrationUsername_SyncExecutionOnPay is
         );
     }
 
-    function test__unit_correct__registrationUsername__nS_PF() external {
+    function test__unit_correct__registrationUsername__noFlush__nS_PF()
+        external
+    {
         addBalance(COMMON_USER_NO_STAKER_1.Address, "test", 0.001 ether);
         makePreRegistrationUsername(
             COMMON_USER_NO_STAKER_1,
@@ -286,7 +477,9 @@ contract unitTestCorrect_NameService_registrationUsername_SyncExecutionOnPay is
         );
     }
 
-    function test__unit_correct__registrationUsername__S_nPF() external {
+    function test__unit_correct__registrationUsername__noFlush__S_nPF()
+        external
+    {
         addBalance(COMMON_USER_NO_STAKER_1.Address, "test", 0);
         makePreRegistrationUsername(
             COMMON_USER_NO_STAKER_1,
@@ -342,7 +535,9 @@ contract unitTestCorrect_NameService_registrationUsername_SyncExecutionOnPay is
         );
     }
 
-    function test__unit_correct__registrationUsername__S_PF() external {
+    function test__unit_correct__registrationUsername__noFlush__S_PF()
+        external
+    {
         addBalance(COMMON_USER_NO_STAKER_1.Address, "test", 0.001 ether);
         makePreRegistrationUsername(
             COMMON_USER_NO_STAKER_1,
@@ -382,6 +577,250 @@ contract unitTestCorrect_NameService_registrationUsername_SyncExecutionOnPay is
 
         (address user, uint256 expirationDate) = nameService
             .getIdentityBasicMetadata("test");
+
+        assertEq(user, COMMON_USER_NO_STAKER_1.Address);
+        assertEq(expirationDate, block.timestamp + 366 days);
+        assertEq(
+            evvm.getBalance(
+                COMMON_USER_NO_STAKER_1.Address,
+                MATE_TOKEN_ADDRESS
+            ),
+            0
+        );
+        assertEq(
+            evvm.getBalance(COMMON_USER_STAKER.Address, MATE_TOKEN_ADDRESS),
+            (evvm.getRewardAmount() * 50) + 0.001 ether
+        );
+    }
+
+    ////////////////////////////////////////////////////////
+
+    function test__unit_correct__registrationUsername__Flush__nS_nPF()
+        external
+        preparePostRegistrationAndFlush
+    {
+        addBalance(COMMON_USER_NO_STAKER_1.Address, "testflush", 0);
+        makePreRegistrationUsername(
+            COMMON_USER_NO_STAKER_1,
+            "testflush",
+            777,
+            10101
+        );
+
+        skip(30 minutes);
+
+        (
+            bytes memory signatureNameService,
+            bytes memory signatureEVVM
+        ) = makeRegistrationUsernameSignatures(
+                COMMON_USER_NO_STAKER_1,
+                "testflush",
+                777,
+                20202,
+                0,
+                evvm.getNextCurrentSyncNonce(COMMON_USER_NO_STAKER_1.Address),
+                false
+            );
+
+        vm.startPrank(COMMON_USER_NO_STAKER_2.Address);
+        nameService.registrationUsername(
+            COMMON_USER_NO_STAKER_1.Address,
+            "testflush",
+            777,
+            20202,
+            signatureNameService,
+            0,
+            evvm.getNextCurrentSyncNonce(COMMON_USER_NO_STAKER_1.Address),
+            false,
+            signatureEVVM
+        );
+        vm.stopPrank();
+
+        (address user, uint256 expirationDate) = nameService
+            .getIdentityBasicMetadata("testflush");
+
+        assertEq(user, COMMON_USER_NO_STAKER_1.Address);
+        assertEq(expirationDate, block.timestamp + 366 days);
+        assertEq(
+            evvm.getBalance(
+                COMMON_USER_NO_STAKER_1.Address,
+                MATE_TOKEN_ADDRESS
+            ),
+            0
+        );
+        assertEq(
+            evvm.getBalance(
+                COMMON_USER_NO_STAKER_2.Address,
+                MATE_TOKEN_ADDRESS
+            ),
+            0
+        );
+    }
+
+    function test__unit_correct__registrationUsername__Flush__nS_PF()
+        external
+        preparePostRegistrationAndFlush
+    {
+        addBalance(COMMON_USER_NO_STAKER_1.Address, "testflush", 0.001 ether);
+        makePreRegistrationUsername(
+            COMMON_USER_NO_STAKER_1,
+            "testflush",
+            777,
+            10101
+        );
+
+        skip(30 minutes);
+
+        (
+            bytes memory signatureNameService,
+            bytes memory signatureEVVM
+        ) = makeRegistrationUsernameSignatures(
+                COMMON_USER_NO_STAKER_1,
+                "testflush",
+                777,
+                20202,
+                0.001 ether,
+                evvm.getNextCurrentSyncNonce(COMMON_USER_NO_STAKER_1.Address),
+                false
+            );
+
+        vm.startPrank(COMMON_USER_NO_STAKER_2.Address);
+        nameService.registrationUsername(
+            COMMON_USER_NO_STAKER_1.Address,
+            "testflush",
+            777,
+            20202,
+            signatureNameService,
+            0.001 ether,
+            evvm.getNextCurrentSyncNonce(COMMON_USER_NO_STAKER_1.Address),
+            false,
+            signatureEVVM
+        );
+        vm.stopPrank();
+
+        (address user, uint256 expirationDate) = nameService
+            .getIdentityBasicMetadata("testflush");
+
+        assertEq(user, COMMON_USER_NO_STAKER_1.Address);
+        assertEq(expirationDate, block.timestamp + 366 days);
+        assertEq(
+            evvm.getBalance(
+                COMMON_USER_NO_STAKER_1.Address,
+                MATE_TOKEN_ADDRESS
+            ),
+            0
+        );
+        assertEq(
+            evvm.getBalance(
+                COMMON_USER_NO_STAKER_2.Address,
+                MATE_TOKEN_ADDRESS
+            ),
+            0
+        );
+    }
+
+    function test__unit_correct__registrationUsername__Flush__S_nPF()
+        external
+        preparePostRegistrationAndFlush
+    {
+        addBalance(COMMON_USER_NO_STAKER_1.Address, "testflush", 0);
+        makePreRegistrationUsername(
+            COMMON_USER_NO_STAKER_1,
+            "testflush",
+            777,
+            10101
+        );
+
+        skip(30 minutes);
+
+        (
+            bytes memory signatureNameService,
+            bytes memory signatureEVVM
+        ) = makeRegistrationUsernameSignatures(
+                COMMON_USER_NO_STAKER_1,
+                "testflush",
+                777,
+                20202,
+                0,
+                evvm.getNextCurrentSyncNonce(COMMON_USER_NO_STAKER_1.Address),
+                false
+            );
+
+        vm.startPrank(COMMON_USER_STAKER.Address);
+        nameService.registrationUsername(
+            COMMON_USER_NO_STAKER_1.Address,
+            "testflush",
+            777,
+            20202,
+            signatureNameService,
+            0,
+            evvm.getNextCurrentSyncNonce(COMMON_USER_NO_STAKER_1.Address),
+            false,
+            signatureEVVM
+        );
+        vm.stopPrank();
+
+        (address user, uint256 expirationDate) = nameService
+            .getIdentityBasicMetadata("testflush");
+
+        assertEq(user, COMMON_USER_NO_STAKER_1.Address);
+        assertEq(expirationDate, block.timestamp + 366 days);
+        assertEq(
+            evvm.getBalance(
+                COMMON_USER_NO_STAKER_1.Address,
+                MATE_TOKEN_ADDRESS
+            ),
+            0
+        );
+        assertEq(
+            evvm.getBalance(COMMON_USER_STAKER.Address, MATE_TOKEN_ADDRESS),
+            evvm.getRewardAmount() * 50
+        );
+    }
+
+    function test__unit_correct__registrationUsername__Flush__S_PF()
+        external
+        preparePostRegistrationAndFlush
+    {
+        addBalance(COMMON_USER_NO_STAKER_1.Address, "testflush", 0.001 ether);
+        makePreRegistrationUsername(
+            COMMON_USER_NO_STAKER_1,
+            "testflush",
+            777,
+            10101
+        );
+
+        skip(30 minutes);
+
+        (
+            bytes memory signatureNameService,
+            bytes memory signatureEVVM
+        ) = makeRegistrationUsernameSignatures(
+                COMMON_USER_NO_STAKER_1,
+                "testflush",
+                777,
+                20202,
+                0.001 ether,
+                evvm.getNextCurrentSyncNonce(COMMON_USER_NO_STAKER_1.Address),
+                false
+            );
+
+        vm.startPrank(COMMON_USER_STAKER.Address);
+        nameService.registrationUsername(
+            COMMON_USER_NO_STAKER_1.Address,
+            "testflush",
+            777,
+            20202,
+            signatureNameService,
+            0.001 ether,
+            evvm.getNextCurrentSyncNonce(COMMON_USER_NO_STAKER_1.Address),
+            false,
+            signatureEVVM
+        );
+        vm.stopPrank();
+
+        (address user, uint256 expirationDate) = nameService
+            .getIdentityBasicMetadata("testflush");
 
         assertEq(user, COMMON_USER_NO_STAKER_1.Address);
         assertEq(expirationDate, block.timestamp + 366 days);
