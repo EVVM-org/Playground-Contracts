f695d41a5c9999c9fbcd37981aa2c80f1cdc0ab4
Manuel Rivera <menu.rivera@gmail.com>
2025-10-30 15:12:03 -0600

    fix: name of unitTestCorrect_P2PSwap_adminTools contract

--- test/unit/p2pSwap/correct/unitTestCorrect_P2PSwap_adminTools.t.sol
+++ test/unit/p2pSwap/correct/unitTestCorrect_P2PSwap_adminTools.t.sol
@@ -30,7 +30,7 @@ import {EvvmStructs} from "@EVVM/playground/contracts/evvm/lib/EvvmStructs.sol";
 import {Treasury} from "@EVVM/playground/contracts/treasury/Treasury.sol";
 import {P2PSwap} from "@EVVM/playground/contracts/p2pSwap/P2PSwap.sol";
 
-contract unitTestCorrect_P2PSwap_cancelOrder is Test, Constants {
+contract unitTestCorrect_P2PSwap_adminTools is Test, Constants {
     Staking staking;
     Evvm evvm;
     Estimator estimator;
